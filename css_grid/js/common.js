46eae9d4490992936d582ab7d1150cd174905d17
Antagonist <t.solomennickov@yandex.ru>
2019-04-13 02:04:40 +0700

    test

--- css_grid/js/common.js
+++ css_grid/js/common.js
@@ -1,5 +1,16 @@
 $(function() {
-	
+	 $(".form-order").submit(function() {
+      $.ajax({
+        type: "POST",
+        url: "../php/mail.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+        $("#form").trigger("reset");
+      });
+      return false;
+    });
 
 
 });
