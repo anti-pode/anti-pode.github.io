91dcf1151ba6b288777b96727759b3b398a127df
Antagonist <t.solomennickov@yandex.ru>
2019-04-13 02:02:39 +0700

    test

---
+++ css_grid/php/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "t.solomennickov@yandex.ru";
+$sitename = "ДоверяевЪ";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$address = trim($_POST["address"]);
+$message = "Имя: $name \nТелефон: $phone \nАдрес: $address";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
